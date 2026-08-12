e284a09b6de2587ea5dd8118499a47ffe83ad22b
Matheus Sousa <rm98856@estudante.fieb.edu.br>
2026-08-12 17:06:39 -0300

    refactor: export app for automated tests

--- app.js
+++ app.js
@@ -2,38 +2,21 @@ const express = require("express");
 const app = express();
 const porta = 3000;
 
-//indicação de pasta de aruivos estáticos - css, js, img 
 app.use(express.static("./app/public"));
-
-//configuar o ejs como mecanismo de renderização
-app.set("view engine","ejs");
-//configurar a pasta das views - html->ejs
-app.set("views","./app/views");
-
-
-//SEMPRE ANTES DA REQUISIÇÃO DAS ROTAS
-//consfiguar o envio de dados e analise de dados na requisição
-//configurar o payload para formato JSON (API)
+app.set("view engine", "ejs");
+app.set("views", "./app/views");
 app.use(express.json());
-//consfiguar o payload para formato URLencoded - (form html -> array, objeto, json)
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
-
-//requisitar arquivos de rotas
 const rota = require("./app/routes/router");
 const rotaAdm = require("./app/routes/router-adm");
-// indicar o local de uso das rotas
 app.use("/", rota);
-// ("/") indica a raiz do site 
-// http://localhost:3000
-app.use("/adm", rotaAdm); 
-// ("/adm") indica acesso ao site pela pasta adm 
-// http://localhost:3000/adm
+app.use("/adm", rotaAdm);
 
-//iniciar o servidor
-app.listen(porta, ()=>{
-    console.log(`Servidor on-line \nhttp://localhost:${porta}`)
-})
+module.exports = app;
 
-//nome1
-//nome2
+if (require.main === module) {
+    app.listen(porta, () => {
+        console.log(`Servidor on-line \nhttp://localhost:${porta}`);
+    });
+}
